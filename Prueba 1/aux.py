437ca277e1daa1e607daaf17858db23ef65d8fab
agustinsilva447 <agustinsilva447@gmail.com>
2020-09-25 17:05:14 -0300

    scigym teleportation bruteforce

--- Prueba 1/aux.py
+++ Prueba 1/aux.py
@@ -1,2 +1,4 @@
-a = range(0,16)
-print(3 in a)
+import numpy as np
+
+T = np.array([[1, 0], [0, 1 / np.sqrt(2) * (1 + 1j)]], dtype=np.complex)
+print(T)
--- Prueba 1/scigym-teleportation-bruteforce.py
+++ Prueba 1/scigym-teleportation-bruteforce.py
@@ -13,20 +13,46 @@ while (reward == 0) and (j<1000) :
     i = 0; j += 1
     done = False
     observation = env.reset()
+    actions_int = []; actions_gate = []
     available = {}; available['available_actions'] = range(num_actions)
 
     while done==False:  
         action = np.random.choice(available['available_actions'])
+        actions_int.append(action)
+        if action == 0:
+            actions_gate.append('H_0')
+        elif action == 1:
+            actions_gate.append('T_0')
+        elif action == 2:
+            actions_gate.append('H_1')
+        elif action == 3:
+            actions_gate.append('T_1')
+        elif action == 4:
+            actions_gate.append('H_2')
+        elif action == 5:
+            actions_gate.append('T_2')
+        elif action == 6:
+            actions_gate.append('CNOT_01')
+        elif action == 7:
+            actions_gate.append('MEASURE_0')
+        elif action == 8:
+            actions_gate.append('MEASURE_1')
+        elif action == 9:
+            actions_gate.append('MEASURE_2')
+
         i += 1
         (observation, reward, done, available) = env.step(action)
         print("{}, {} --> Reward: {}. Done: {}. Available actions: {}.".format(j, i, reward, done, available['available_actions']), end=" ")
         
         if bool(available['available_actions']):
-            print("Action {}: {}.".format(i, action))
+            print("Action: {}. Gate: {}".format(action, actions_gate[i-1]))
         else:
             print("No more available actions.")
 
     if reward == 0:
         print("Target not met.")
     elif reward == 1:
-        print("Congratulations!")
+        print("Number of actions: {}.".format(i))
+        #print("Actions (numbs): {}.".format(actions_int))
+        print("Actions (gates): {}.".format(actions_gate))
+        #print("Congratulations!")
