fe9aa0487ad4c215174377b5705118731ad49a2e
agustinsilva447 <agustinsilva447@gmail.com>
2020-09-25 22:30:47 -0300

    scigym cirq teleportation bruteforce

--- Prueba 1/scigym-teleportation-bruteforce.py
+++ Prueba 1/scigym-teleportation-bruteforce.py
@@ -1,31 +1,36 @@
-import scigym
 import numpy as np
-
+import scigym
 import cirq
 from cirq.ops import H, T, CNOT, measure
 from cirq.circuits import InsertStrategy
 
 j = 0; reward = 0
-
 while (reward == 0) and (j<1000) :
-    env = scigym.make('teleportation-v0')
-    if isinstance(env.action_space,scigym.Discrete):
-        num_actions = env.action_space.n
-    else:
-        raise ValueError
-
     i = 0; j += 1
+    env = scigym.make('teleportation-v0')  
+    observation = env.reset()  
     done = False
-    observation = env.reset()
-    actions_int = []; actions_gate = []
-    available = {}; available['available_actions'] = range(num_actions)
-
-    q0, q1, q2 = [cirq.GridQubit(i, 0) for i in range(3)]
-    circuit = cirq.Circuit()
-
+    actions_int = []
+    num_actions = env.action_space.n; available = {}
+    available['available_actions'] = range(num_actions)
     while done==False:  
+        i += 1
         action = np.random.choice(available['available_actions'])
         actions_int.append(action)
+        (observation, reward, done, available) = env.step(action)
+        print("{}, {} --> Reward: {}. Done: {}. Available actions: {}.".format(j, i, reward, done, available['available_actions']), end=" ")      
+        if bool(available['available_actions']):
+            print("Action: {}.".format(action))
+        else:
+            print("No more available actions.")
+    if reward == 0:
+        print("Target not met.")
+
+if reward == 1:
+    actions_gate = []
+    q0, q1, q2 = [cirq.GridQubit(i, 0) for i in range(3)]
+    circuit = cirq.Circuit()
+    for action in actions_int:
         if action == 0:
             actions_gate.append('H_0')
             circuit.append([H(q0)], strategy=InsertStrategy.NEW)
@@ -55,23 +60,7 @@ while (reward == 0) and (j<1000) :
             circuit.append([measure(q1)], strategy=InsertStrategy.NEW)
         elif action == 9:
             actions_gate.append('MEASURE_2')
-            circuit.append([measure(q2)], strategy=InsertStrategy.NEW)
+            circuit.append([measure(q2)], strategy=InsertStrategy.NEW)            
 
-        i += 1
-        (observation, reward, done, available) = env.step(action)
-        print("{}, {} --> Reward: {}. Done: {}. Available actions: {}.".format(j, i, reward, done, available['available_actions']), end=" ")
-        
-        if bool(available['available_actions']):
-            print("Action: {}. Gate: {}".format(action, actions_gate[i-1]))
-        else:
-            print("No more available actions.")
-
-    if reward == 0:
-        print("Target not met.")
-    elif reward == 1:
-        print("Number of actions: {}. Actions (gates): {}.".format(i, actions_gate))
-        print(circuit)
-        #print("Actions (numbs): {}.".format(actions_int))
-        #print("Congratulations!")
-
-##########
+    print("\nNumber of actions: {}. Actions (gates): {}.\nQuantum Circuit:".format(i, actions_gate))
+    print(circuit)
