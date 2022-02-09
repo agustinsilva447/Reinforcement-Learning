9efcce17f5f128a26a7eff854587ab14506d714d
agustinsilva447 <agustinsilva447@gmail.com>
2022-02-08 23:52:36 -0300

    qrl

---
+++ Lockwood/qdqn.py
@@ -0,0 +1,13 @@
+import gym
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import math
+from collections import deque
+import tensorflow as tf
+import tensorflow_quantum as tfq
+import cirq 
+import sympy
+
+
+print("Hello quantum!")
