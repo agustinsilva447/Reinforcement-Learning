e5f51c5b7e0dc1b5f503b8df718adefbedb7096e
agustinsilva447 <agustinsilva447@gmail.com>
2022-02-14 01:48:27 -0300

    VQC-QGT

--- QRL/qgrad/net_VQC.py
+++ QRL/qgrad/net_VQC.py
@@ -5,7 +5,6 @@ from jax import grad
 
 import matplotlib.pyplot as plt
 from functools import reduce
-from qgrad.qgrad_qutip import basis, fidelity
 
 def rx(phi):
     return jnp.array([[jnp.cos(phi / 2), -1j * jnp.sin(phi / 2)],
@@ -35,20 +34,21 @@ def J_dag():
                          
 def circuit(params):
     thetax, thetay, thetaz = params
-    layer0 = jnp.kron(basis(2, 0), basis(2, 0))
+    layer0 = jnp.array([[1], [0], [0], [0]])
     layer1 = J()
     layer2 = jnp.kron(rx(thetax), rx(thetax))
     layer3 = jnp.kron(ry(thetay), ry(thetay))
     layer4 = jnp.kron(rz(thetaz), rz(thetaz))
     layer5 = J_dag()
-    layers = [layer5, layer4, layer3, layer2, layer1]
-    unitary = reduce(lambda  x, y : jnp.dot(x, y), layers)
-    return jnp.dot(unitary, layer0)  
+    layers = [layer5, layer4, layer3, layer2, layer1, layer0]
+    q_state_out = reduce(lambda  x, y : jnp.dot(x, y), layers)
+    return q_state_out
 
 def cost(params):
     state = circuit(params)
-    op_qs = jnp.array([[0], [0.707],   [0.707],   [0]])
-    return -jnp.real(fidelity(op_qs,state))[0][0]
+    op_qs = jnp.array([[0], [0.707], [0.707], [0]])
+    fid = jnp.abs(jnp.dot(jnp.transpose(jnp.conjugate(op_qs)), state)) ** 2
+    return -jnp.real(fid)[0][0]
 
 # fixed random parameter initialization
 init_params = [2 * jnp.pi * np.random.rand(), 2 * jnp.pi * np.random.rand(), 2 * jnp.pi * np.random.rand()]
@@ -60,24 +60,29 @@ def step(i, opt_state, opt_update):
     g = grad(cost)(params)
     return opt_update(i, g, opt_state)
 
-epochs = 150
-loss_hist = []
+epoch = 0
+epoch_max = 250
 
-for epoch in range(epochs):
+loss_hist = []
+loss = cost(init_params)
+
+while ((loss > -0.999) and (epoch_max > epoch)): 
     opt_state = step(epoch, opt_state, opt_update)
     params = get_params(opt_state)
     loss = cost(params)
     loss_hist.append(loss)
     progress = [epoch+1, loss]
     if (epoch % 50 == 49):
-        print("Epoch: {:2f} | Loss: {:3f}".format(*jnp.asarray(progress)))
+        print("Epoch: {} | Loss: {}".format(*jnp.asarray(progress)))
+    epoch += 1
+print("Epoch: {} | Loss: {}".format(*jnp.asarray(progress)))
 
 print("\nBest action: [Rx, Ry, Rz] = [{}, {}, {}]".format(params[0], params[1], params[2]))   
-d = np.round((np.array(params))/(np.pi),2)
-print("Best action: [Rx, Ry, Rz] = [{}*π, {}*π, {}*π]\n".format(d[0], d[1], d[2])) 
+d = (np.array(params))/(np.pi)
+print("Best action: [Rx, Ry, Rz] = [{:.2f}*π, {:.2f}*π, {:.2f}*π]\n".format(d[0], d[1], d[2])) 
 
 output = circuit(params)
-print("Quantum state output = {}.\n".format(np.round(output,3)))    
+print("Quantum state output = {}.\n".format(output))    
 
 plt.plot(loss_hist)
 plt.ylabel("Loss")
